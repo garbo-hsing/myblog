57f61efadf1e53054577dcb16d9ad3e1a10874f1
garbohsing <garbohsing@gmail.com>
2021-01-13 18:00:57 +0800

    JWT功能实现

---
+++ backend/src/main/java/com/garbo/interceptors/JWTInterceptor.java
@@ -0,0 +1,18 @@
+package com.garbo.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author garbo
+ * @description JWTInterceptor
+ * @create 2021-01-13 17:58
+ */
+public class JWTInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+}
---
+++ backend/src/main/java/com/garbo/utils/JWTUtils.java
@@ -0,0 +1,9 @@
+package com.garbo.utils;
+
+/**
+ * @author garbo
+ * @description JWT工具类
+ * @create 2021-01-13 18:00
+ */
+public class JWTUtils {
+}
